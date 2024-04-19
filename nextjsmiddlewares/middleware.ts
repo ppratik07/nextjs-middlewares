259de8e410eb75da5c98ee5ca5eef50e99943140
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-04-19 21:44:04 +0530

    created a middleware

---
+++ nextjsmiddlewares/middleware.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+let requestCount = 0;
+export function middleware(req: NextRequest){
+    requestCount++;
+    const res = NextResponse.next()
+    return res;
+}
