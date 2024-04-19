9a2f9484974b9c26016c9c97a374b02a1e4d7af4
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-04-19 21:53:14 +0530

    more effective way of doing selectivily middleware

--- nextjsmiddlewares/middleware.ts
+++ nextjsmiddlewares/middleware.ts
@@ -1,19 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 
-let requestCount = 0;
-export function middleware(req: NextRequest){
-    requestCount++;
-    const res = NextResponse.next()
-    console.log("request count ",requestCount);
-    return res;
-}
+// let requestCount = 0;
+// export function middleware(req: NextRequest){
+//     requestCount++;
+//     const res = NextResponse.next()
+//     console.log("request count ",requestCount);
+//     return res;
+// }
 
 //Issue- NextJS does file based routing sp This runs on ALL Routes 
 
 //selectivily
 
-export const config = {
-    matcher: '/api/:path'
-}
+// export const config = {
+//     matcher: '/api/:path'
+// }
 
 //but this runs on only 1 page filter
+ 
+export function middleware(request: NextRequest) {
+  console.log(request.nextUrl.pathname)
+  if (request.nextUrl.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(new URL('/signin', request.url))
+  }
+ 
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.next()
+  }
+}
